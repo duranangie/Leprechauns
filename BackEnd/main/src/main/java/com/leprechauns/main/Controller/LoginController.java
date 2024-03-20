62c1ddca1b13612a1d2a3aadb8bd353dd619323f
duranangie <117625258+duranangie@users.noreply.github.com>
2024-03-20 00:12:34 -0500

    fix: :bug: add token visibility in controller
    add token visibility in postman

--- BackEnd/main/src/main/java/com/leprechauns/main/Controller/LoginController.java
+++ BackEnd/main/src/main/java/com/leprechauns/main/Controller/LoginController.java
@@ -1,49 +1,51 @@
-package com.leprechauns.main.Controller;
+    package com.leprechauns.main.Controller;
+    
+    import java.util.Map;
+    
+    import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.access.prepost.PreAuthorize;
+    import org.springframework.security.core.userdetails.UsernameNotFoundException;
+    import org.springframework.web.bind.annotation.CrossOrigin;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
+    import org.springframework.web.bind.annotation.RestController;
+    
+    import com.leprechauns.main.security.JWTAuthorizationConfig;
+    import com.leprechauns.main.Entity.UserProject;
+    import com.leprechauns.main.Entity.DTO.UserProjectDTO;
+    import com.leprechauns.main.Service.UserProjectService;
+    
+    
+    @RestController
+    @CrossOrigin("*")
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "bearerAuth")
+    public class LoginController {
+        @Autowired
+        JWTAuthorizationConfig jwtAuthenticationConfig;
+    
+        @Autowired
+        UserProjectService userService;
+    
+        @PostMapping("/login")
+        public UserProject login(
+                @RequestParam("user") String username,
+                @RequestParam("encryptedPass") String encryptedPass) {
+    
+            userService.userValidation(username, encryptedPass);
+            String token = jwtAuthenticationConfig.getJWTToken(username);
+            return new UserProject(username, encryptedPass, token);
+        }
+    
+        @PostMapping("/register")
+        public ResponseEntity<Map<Object, Object>> registerUser(
+                @RequestParam("user") String username,
+                @RequestParam("encryptedPass") String encryptedPass) {
 
-import java.util.Map;
+            Map<Object, Object> response = userService.registerUser(new UserProject(username, encryptedPass, null));
+            return ResponseEntity.ok(response);
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.leprechauns.main.security.JWTAuthorizationConfig;
-import com.leprechauns.main.Entity.UserProject;
-import com.leprechauns.main.Entity.DTO.UserProjectDTO;
-import com.leprechauns.main.Service.UserProjectService;
-
-
-@RestController
-@CrossOrigin("*")
-@PreAuthorize("hasRole('ADMIN')")
-@SecurityRequirement(name = "bearerAuth")
-public class LoginController {
-    @Autowired
-    JWTAuthorizationConfig jwtAuthenticationConfig;
-
-    @Autowired
-    UserProjectService userService;
-
-    @PostMapping("/login")
-    public UserProject login(
-            @RequestParam("user") String username,
-            @RequestParam("encryptedPass") String encryptedPass) {
-
-        userService.userValidation(username, encryptedPass);
-        String token = jwtAuthenticationConfig.getJWTToken(username);
-        return new UserProject(username, encryptedPass, token);
+        }
     }
-
-    @PostMapping("/register")
-    public Map<Object, Object> registerUser(
-            @RequestParam("user") String username,
-            @RequestParam("encryptedPass") String encryptedPass) {
-
-        return userService.registerUser(new UserProject(username, encryptedPass, null));
-    }
-}
